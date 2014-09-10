113fb818b1aa57267b5152f0db622957d359f8d9
Coding-House <instructor@codinghouse.co>
2014-09-10 14:14:21 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,45 +1,5 @@
 #!/usr/bin/env bash
 
-GENERAL_INSTALLS=true
-
-################# APT
-
-if [[ "$GENERAL_INSTALLS" = "true" ]]; then
-  echo "apt..."
-  sudo apt-get update
-  sudo apt-get upgrade -y
-  sudo apt-get autoremove -y
-  sudo apt-get autoclean -y
-  sudo apt-get install -y autoconf automake build-essential curl exuberant-ctags g++ git iproute libreadline-dev libreadline6 libreadline6-dev ncurses-dev openssl pkg-config portmap rlwrap tmux vim whois nodejs npm
-fi
-
-################# SOLARIZED
-
-if [[ ! -f $HOME/.dircolors ]]; then
-  cd $HOME
-  wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-light
-  mv dircolors.ansi-light .dircolors
-  eval `dircolors ~/.dircolors`
-fi
-
-################# RVM/RUBY
-
-echo "rvm..."
-if [[ ! -d $HOME/.rvm ]]; then
-  curl -L get.rvm.io | bash -s stable
-else
-  rvm get stable
-fi
-
-################# .files/GIT
-
-echo ".files..."
-if [[ ! -d $HOME/.files ]]; then
-  git clone https://github.com/samerbuna/.files.git $HOME/.files
-else
-  cd $HOME/.files && git pull --rebase
-fi
-
 cd $HOME
 
 echo "Linking dotfile..."
